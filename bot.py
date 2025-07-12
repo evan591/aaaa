7bc912c62174d0157834d858e2648c4fc29f0513
evan591 <aaae56560@gmail.com>
2025-07-12 21:58:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -383,7 +383,6 @@ async def restore(interaction: discord.Interaction, file: discord.Attachment):
 
     await interaction.followup.send(f"✅ 復元が完了しました！ ({len(messages_data)} 件)", ephemeral=True)
 
-いません", ephemeral=True)
     
 # =====================
 # 起動処理
